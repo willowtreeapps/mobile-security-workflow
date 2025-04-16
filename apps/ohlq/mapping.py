3c69fb86c4a9b796670d08ad679f57b9b15ab7b1
cassio-santos-ptk <cassio.santos@poatek.com>
2025-04-16 09:29:14 -0300

    add login test

--- apps/ohlq/mapping.py
+++ apps/ohlq/mapping.py
@@ -5,9 +5,9 @@ import os
 # -    load the .env file   -
 load_dotenv()
 
-# -     DEFINE THE MOCK DATA    -
-#MOCK_USR = os.getenv("MOCK_USER_NAME")
-#MOCK_PASWD = os.getenv("MOCK_PASSWORD")
+#-     DEFINE THE MOCK DATA    -
+MOCK_USR = os.getenv("MOCK_USER_NAME")
+MOCK_PASWD = os.getenv("MOCK_PASSWORD")
 
 #Sign up
 MOCK_EMAIL = os.getenv("MOCK_EMAIL")
@@ -22,51 +22,51 @@ MOCK_LASTNAME = os.getenv("MOCK_LASTNAME")
 
 """
 
-#def login():
-    #click on red button
-   # do_tap(724, 206)
-    # type accont
-   # do_input_text(MOCK_USR)
-    # type continue
-  #  do_tap(733, 204)
-    # click input password
-   # do_tap(724, 780)
-    # type password
-  #  do_input_text(MOCK_PASWD)
-    # click continue button
-   # do_tap(721, 2230)
+def login():
+  #click on red button
+  do_tap(724, 206)
+  # type accont
+  do_input_text(MOCK_USR)
+  # type continue
+  do_tap(733, 204)
+  #click input password
+  do_tap(724, 780)
+  #type password
+  do_input_text(MOCK_PASWD)
+  #click continue button
+  do_tap(721, 2230)
 
-def signup():
+# def signup():
 
-    #click on Create An Account button
-    do_tap(377, 1058)
-    # type email
-    do_input_text(MOCK_EMAIL)
-    # press continue
-    do_tap(373, 1000)
-    # type First name
-    do_input_text(MOCK_FIRSTNAME)
-    # press password
-    do_tap(358, 580)
-     # type last name
-    do_input_text(MOCK_LASTNAME)
-    # press continue
-    do_tap(382, 1007) # issue solved
-    # type date
-    do_input_text(MOCK_DATE)
-    # press continue      
-    do_tap(404, 1044)
-    # click password field 
-    do_tap(378, 486)
-    # input password 
-    do_input_text(MOCK_PASSWORD_SIGNUP)
-    # press Confirm Password
-    do_tap(381, 619)
-    # input confirm password 
-    do_input_text(MOCK_PASSWORD_SIGNUP)
-    # # press continue
-    do_tap(396, 1053)
+#     #click on Create An Account button
+#     do_tap(377, 1058)
+#     # type email
+#     do_input_text(MOCK_EMAIL)
+#     # press continue
+#     do_tap(373, 1000)
+#     # type First name
+#     do_input_text(MOCK_FIRSTNAME)
+#     # press password
+#     do_tap(358, 580)
+#      # type last name
+#     do_input_text(MOCK_LASTNAME)
+#     # press continue
+#     do_tap(382, 1007) # issue solved
+#     # type date
+#     do_input_text(MOCK_DATE)
+#     # press continue      
+#     do_tap(404, 1044)
+#     # click password field 
+#     do_tap(378, 486)
+#     # input password 
+#     do_input_text(MOCK_PASSWORD_SIGNUP)
+#     # press Confirm Password
+#     do_tap(381, 619)
+#     # input confirm password 
+#     do_input_text(MOCK_PASSWORD_SIGNUP)
+#     # # press continue
+#     do_tap(396, 1053)
 
-    do_sleep(20)
+#     do_sleep(20)
 
 
