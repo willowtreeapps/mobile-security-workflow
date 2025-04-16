c973d66a92af2a840f6210a16bdd253269188b25
Bejon D Iscandri <108363908+Beejiguy@users.noreply.github.com>
2025-04-16 14:00:58 -0500

    Update manager.py

--- apps/ohlq/manager.py
+++ apps/ohlq/manager.py
@@ -28,18 +28,18 @@ def do_test(package):
 
     vuln_service.check_emulator(package)
 
-    # --- Login function ------
+#    # --- Login function ------
 
-    ohlq.login()
+ #   ohlq.login()
 
-    vuln_service.search_shared_pref(MOCK_USR, package)
+ #   vuln_service.search_shared_pref(MOCK_USR, package)
 
-    vuln_service.search_sqlite(MOCK_USR, package)
+#    vuln_service.search_sqlite(MOCK_USR, package)
 
-    vuln_service.search_sqlite(MOCK_PASWD, package)
+ #   vuln_service.search_sqlite(MOCK_PASWD, package)
 
-    vuln_service.search_sensitive_log(MOCK_USR)
-    vuln_service.search_sensitive_log(MOCK_PASWD)
+#    vuln_service.search_sensitive_log(MOCK_USR)
+  #  vuln_service.search_sensitive_log(MOCK_PASWD)
 
     # ---- Sinup function -------
 
