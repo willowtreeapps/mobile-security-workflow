013a8a5bb4a4ef7d05f4c0878242b666fdceee6c
cassio-santos-ptk <cassio.santos@poatek.com>
2025-06-12 16:30:56 -0300

    add ohlq dev

--- apps/ohlq/manager.py
+++ apps/ohlq/manager.py
@@ -6,29 +6,33 @@ import os
 
 load_dotenv()
 
-
 def do_test(package):
     # -     Define the Mock Data    -
     MOCK_USR = os.getenv("MOCK_USER_NAME")
     MOCK_PASWD = os.getenv("MOCK_PASSWORD")
 
-    # -     Open the App     -
+    # -     Open the App
     do_open(package)
 
+    # -     Root and Emulator checks
     vuln_service.check_root(package)
-
     vuln_service.check_emulator(package)
 
-    # ohlq.login()
+    # -     Login in the application
+    ohlq.login()
 
-    # vuln_service.search_shared_pref(MOCK_PASWD, package)
+    # -     Look to Sensitive data in Shared Pref
+    vuln_service.search_shared_pref(MOCK_PASWD, package)
+    
+    # -     Look to Sensitive data in SQLite
+    vuln_service.search_sqlite(MOCK_USR, package)
 
-    # vuln_service.search_sqlite(MOCK_USR, package)
-
-    # # -     Look to Sensitive data in Logs
-    # vuln_service.search_sensitive_log(MOCK_USR)
-
-    # vuln_service.search_sensitive_log(MOCK_PASWD)
+    # -     Look to Sensitive data in Logs
+    vuln_service.search_sensitive_log(MOCK_USR)
+    vuln_service.search_sensitive_log(MOCK_PASWD)
+    
+    #       Look to Sensitive data in external storage
+    vuln_service.search_sensitive_external(MOCK_USR)
 
     # -     Create the .sarif File Report
     vuln_service.build_report()
