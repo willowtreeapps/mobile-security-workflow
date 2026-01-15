b8c7744c682e342f95ac5c76fe4a987d1f4c61c5
cassio-santos-ptk <cassio.santos@poatek.com>
2026-01-15 11:45:03 -0300

    add dfd app

---
+++ apps/dfd/manager.py
@@ -0,0 +1,54 @@
+from dotenv import load_dotenv
+from service.input_service import do_restart, do_open, do_close
+from service import vulnerability_service as vuln_service
+from apps.dfd import mapping as dfd
+import os
+
+import time
+
+""""
+
+    DFD test manager file. All the test structure for this app
+    is centralized here.
+
+"""
+
+load_dotenv()
+
+def do_test(package):
+    # -     Define the Mock Data    -
+    MOCK_USR = os.getenv("MOCK_USER_NAME") 
+    MOCK_PASWD = os.getenv("MOCK_PASSWORD")
+    MOCK_EMAIL = os.getenv("MOCK_EMAIL")
+    MOCK_FIRST_NAME = os.getenv("MOCK_FIRST_NAME")
+
+    # -     Open the App     -
+    do_open(package)        
+
+    vuln_service.check_root(package)
+
+    vuln_service.check_emulator(package)
+    
+    # -     Give the pentester some time to chat a bit (3 minutes)   -
+    time.sleep(180)
+    
+    vuln_service.search_sensitive_external(MOCK_USR)        
+
+    # -     Search for Vulnerabilities       -
+    vuln_service.search_shared_pref(MOCK_PASWD, package)
+
+
+    # -     Look to Sensitive Data at SQLite
+    vuln_service.search_sqlite(MOCK_EMAIL, package)
+  
+
+    # -     Look to Sensitive data in Logs
+    vuln_service.search_sensitive_log(MOCK_FIRST_NAME)    
+    vuln_service.search_sensitive_log(MOCK_EMAIL)
+    vuln_service.search_sensitive_log(MOCK_PASWD)        
+
+    # -     Create the .sarif File Report
+    vuln_service.build_report()
+
+    do_close(package)
+
